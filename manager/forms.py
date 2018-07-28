cdd06a478fcc093ca0119d0d42378e3cd18950b6
developper <daichi.hata.19921003@gmail.com>
2018-07-28 17:33:59 +0900

    test

--- manager/forms.py
+++ manager/forms.py
@@ -18,6 +18,13 @@ FOOD_CHOICES = (
 
 )
 
+class HelloForm(forms.Form):
+    your_name = forms.CharField(
+        label='名前',
+        max_length=20,
+        required=True,
+        widget=forms.TextInput()
+    )
 
 class SampleForm(forms.Form):
     age = forms.IntegerField(
